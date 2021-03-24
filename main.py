e9e195a1632d71ad9fd73d5fbd0e351dd75faa93
Jittat Fakcharoenphol <jittat@gmail.com>
2021-03-24 22:53:34 +0700

    Show enemy

--- main.py
+++ main.py
@@ -1,81 +1,13 @@
 import math
-from random import randint
+from random import randint, random
 
 import tkinter as tk
 
 from gamelib import Sprite, GameApp, Text
 
-CANVAS_WIDTH = 800
-CANVAS_HEIGHT = 600
-
-UPDATE_DELAY = 33
-
-SHIP_SPEED = 5
-SHIP_TURN_ANGLE = 5
-BULLET_BASE_SPEED = 10
-
-def direction_to_dxdy(direction):
-    return (math.cos(direction * math.pi / 180), 
-        math.sin(direction * math.pi / 180))
-
-
-class Bullet(Sprite):
-    def __init__(self, app, x, y, vx, vy):
-        super().__init__(app, 'images/bullet1.png', x, y)
-        self.vx = vx
-        self.vy = vy
-
-    def update(self):
-        self.x += self.vx
-        self.y += self.vy
-
-
-class Ship(Sprite):
-    def __init__(self, app, x, y):
-        super().__init__(app, 'images/ship.png', x, y)
-
-        self.app = app
-
-        self.direction = 0
-        self.is_turning_left = False
-        self.is_turning_right = False
-
-    def update(self):
-        dx,dy = direction_to_dxdy(self.direction)
-
-        self.x += dx * SHIP_SPEED
-        self.y += dy * SHIP_SPEED
-
-        if self.is_turning_left:
-            self.turn_left()
-        elif self.is_turning_right:
-            self.turn_right()
-
-    def start_turn(self, dir):
-        if dir.upper() == 'LEFT':
-            self.is_turning_left = True
-            self.is_turning_right = False
-        else:
-            self.is_turning_right = True
-            self.is_turning_left = False
-
-    def stop_turn(self, dir=None):
-        if (dir == None) or (dir.upper() == 'LEFT'):
-            self.is_turning_left = False
-        if (dir == None) or (dir.upper() == 'RIGHT'):
-            self.is_turning_right = False
-
-    def turn_left(self):
-        self.direction -= SHIP_TURN_ANGLE
-
-    def turn_right(self):
-        self.direction += SHIP_TURN_ANGLE
-
-    def fire(self):
-        dx,dy = direction_to_dxdy(self.direction)
-        bullet = Bullet(self.app, self.x, self.y, dx * BULLET_BASE_SPEED, dy * BULLET_BASE_SPEED)
-        self.app.elements.append(bullet)
-
+from consts import *
+from elements import Ship, Bullet, Enemy
+from utils import random_edge_position, normalize_vector
 
 class SpaceGame(GameApp):
     def init_game(self):
@@ -83,8 +15,19 @@ class SpaceGame(GameApp):
 
         self.elements.append(self.ship)
 
+    def add_enemy(self):
+        x, y = random_edge_position()
+        vx, vy = normalize_vector(self.ship.x - x, self.ship.y - y)
+
+        vx *= ENEMY_BASE_SPEED
+        vy *= ENEMY_BASE_SPEED
+
+        enemy = Enemy(self, x, y, vx, vy)
+        self.elements.append(enemy)
+
     def pre_update(self):
-        pass
+        if random() < 0.1:
+            self.add_enemy()
 
     def post_update(self):
         pass
